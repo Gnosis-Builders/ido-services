bf69795d97467b96c1b27947d32abee5f766e6cf
josojo <josojo@hotmail.de>
2021-02-01 08:46:32 +0100

    renaming Auctiondetails with serde uppercase

--- model/src/auction_details.rs
+++ model/src/auction_details.rs
@@ -6,6 +6,7 @@ use std::cmp::Ordering;
 use std::cmp::PartialOrd;
 
 #[derive(Clone, Debug, Default, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct AuctionDetails {
     pub auction_id: u64,
     pub order: PricePoint,
