d85cf3a54a2f69d262842ca03dfb0fc53e63a9a1
AlexHerrmann(josojo) <josojo@hotmail.de>
2021-03-17 17:31:38 +0100

    Modifications for deployment (#61)
    * testing deployment
    
    * anotthe rttry
    
    * loading the env variable
    
    * testing
    
    * more testing
    
    * new trail
    
    * different tag handover
    
    * different tag handover

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -7,19 +7,16 @@ image_name=$1
 sudo apt-get update && sudo apt-get install -y python3-pip python3-setuptools && pip3 install --upgrade --user awscli
 
 # Get login token and execute login
-$(aws ecr get-login --no-include-email --region $AWS_REGION)
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 echo "Tagging latest private image for orderbook...";
-docker build --tag $REGISTRY_URI:$image_name  -f docker/Dockerfile.binary .
+docker build --tag $REGISTRY_URI:$image_name -f docker/Dockerfile.binary .
 
 echo "Pushing private image";
 docker push $REGISTRY_URI:$image_name
 
-echo "The private image has been pushed";
-rm -rf .ssh/*
-
 echo "Tagging latest private image for db-migrations...";
-docker build -t $REGISTRY_URI-migrations -f docker/Dockerfile.migration .
+docker build --tag $REGISTRY_URI-migrations:$image_name -f docker/Dockerfile.migration .
 
 echo "Pushing private image";
 docker push $REGISTRY_URI-migrations:$image_name
